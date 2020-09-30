0505dcbb13c589c9436d67c670689fa6c55ef059
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-09-29 21:24:41 -0700

    update largest_smallest

--- ex5.3_larrgest_smallest/AdrianA_solution.py
+++ ex5.3_larrgest_smallest/AdrianA_solution.py
@@ -1,15 +1,15 @@
 num_list = []
-# largest = big_num   #already commented out
-# smallest = small_num    #already commented out
 
-try:
-    while True:
-        num = input("Enter a number. If done, type 'done'.")
+while True:
+    num = input("Enter a number. If done, type 'done'.")
+    if num == 'done':
+        break
+    try:
+        num = int(num)
         num_list.append(num)
-        if num == 'done':
-            break
-except:
-    print('Invalid input')
+
+    except:
+        print('Invalid input')
 
 print('Maximum is ', max(num_list))
 print('Minimum is ', min(num_list))
--- ex5.3_larrgest_smallest/ChienT_solution.py
+++ ex5.3_larrgest_smallest/ChienT_solution.py
@@ -1,25 +1,22 @@
 # Chien's solution
 
-# change order to be able to add all 5 inputs before program exits
 int_list = []
-# move next 2 lines after while loop
-# print(f'Maximum is {largest}')
-# print(f'Minimum is {smallest}')
 
-try:
-    while True:
-        num = int(input('Enter a number: '))
-        if num == 'done':
-            break
+while True:
+    num = input('Enter a number: ')
+    if num == 'done':
+        break
+    try:
+        num = int(num)
         # int_list()   # needs argument
         int_list.append(num)
-except:
-    print('Invalid input')
+    except:
+        print('Invalid input')
 
 largest = max(int_list)
 smallest = min(int_list)
-print(f'Maximum is {largest}')
-print(f'Minimum is {smallest}')
-print(int_list)
+print("Maximum is", largest)
+print("Minimum is", smallest)
+
 
 
